f60228a0cdf5fc29917345fcaeae7fc83b29aefd
ProfSensei <janarieloc@gmail.com>
2019-05-08 15:31:02 +0200

    For testing purposes

---
+++ dictionary_test.py
@@ -0,0 +1,23 @@
+#MRMenu = {}
+
+#signal = input("Type here: ")
+#data = input("Data here: ")
+
+
+#MRMenu[signal] = data
+#print(MRMenu[signal])
+
+
+#for x, y in MRMenu.items():
+#  print(x, y) 
+
+Waifu = {}
+
+# because git told me to :(
+
+for i in range(0,5):
+    potato = input("This is a potato")
+    Waifu[i] = potato
+
+for x,y in Waifu.items():
+    print(x,y)
