a7a240249db72752ac7778935419b729882df216
ProfSensei <janarieloc@gmail.com>
2019-05-21 23:31:01 +0200

    Canny-opencv

---
+++ CV-collection/Canny-Edge/Canny01.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('01.jpg',cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
